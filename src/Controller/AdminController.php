5c47691bd5582ab36f3a85533653ffe0f540db35
Benjamin Coajou <coajou.benjamin@gmail.com>
2021-05-11 14:54:12 +0200

    action editer sur admin utilisateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Training;
 use App\Entity\User;
+use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,5 +23,26 @@ class AdminController extends AbstractController
         $users = $repoUser->findAll();
         $trainings = $repoTraining->findAll();
         return $this->render('admin/index.html.twig', compact('users', 'trainings'));
+    }
+
+    /**
+     * @Route("/admin/user/edit/{user}", name="admin_user_edit")
+     */
+    public function editUser(Request $request, User $user, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('admin/user/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
