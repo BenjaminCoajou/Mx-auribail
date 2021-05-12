5e5e54b4c894557113567ec02eb912030bf4b3c3
François <francois.rivolet@hotmail.fr>
2021-05-12 14:43:26 +0200

    Creation MailerController

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/email/{user}", name="email")
+     */
+    public function sendEmail(MailerInterface $mailer, User $user): Response
+    {
+        $email = (new Email())
+        ->from('mx-auribail@gmail.com')
+        ->to($user->getEmail())
+        ->subject('Test mailer')
+        ->text('Le bon test Vroom Vroom !');
+
+        $mailer->send($email);
+        
+        return $this->redirectToRoute('home');
+        
+    }
+}
