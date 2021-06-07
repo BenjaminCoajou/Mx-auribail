6de03cf7cdf98d1e9e08c329e8fa985be61773a2
Benjamin Coajou <coajou.benjamin@gmail.com>
2021-06-07 15:08:39 +0200

    display user position in waiting list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,24 +11,27 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, ?UserInterface $user)
     {
         $repoTraining = $em->getRepository(Training::class);
-        $trainingAdulte = $repoTraining->findOneBy([
-            'adult' => 1        
-        ], 
-        [
-            'trainingDate' => 'desc'
-        ]);
+        $trainingAdulte = $repoTraining->findOneBy(
+            [
+                'adult' => 1
+            ],
+            [
+                'trainingDate' => 'desc'
+            ]
+        );
 
         $trainingEnfant = $repoTraining->findOneBy([
-            'adult' => 0       
+            'adult' => 0
         ], [
             'trainingDate' => 'desc'
         ]);
@@ -40,12 +43,54 @@ class HomeController extends AbstractController
 
         $userTrainingEnfant = $repoUserTraining->findBy([
             'training' => $trainingEnfant
+        ], [
+            'dateRegistration' => 'desc'
         ]);
+
+        if ($user) {
+
+            $userId = $user->getId();
+            $userEmail = $user->getEmail();
+            $training = $repoUserTraining->findOneBy([
+                'user' => $userId
+            ]);
+            $list = $repoUserTraining->findAll([
+                'training' => $training->getId()
+            ]);
+            if ($training) {
+                $currentTraining = $training->getTraining();
+                $slot = $currentTraining->getSlot();
+                for ($i = 0; $i < count($list); $i++) {
+                    
+                    if ($list[$i]->getUser()->getEmail() == $userEmail) {
+                        $userPlace = $i + 1;
+                    }
+                }
+
+                if($userPlace < $slot) {
+                    $nameList = "principale";
+                    
+                }
+                else {
+                    $nameList ="d'attente";
+                }
+                
+            } else {
+                $nameList = "";
+            }
+        } else {
+
+            $nameList = "";
+            $userPlace = "";
+        }
+
+
+
 
         $placeAdult = count($userTrainingAdult);
         $placeEnfant = count($userTrainingEnfant);
 
-        return $this->render('home/index.html.twig', compact('trainingAdulte', 'trainingEnfant', 'placeAdult', 'placeEnfant'));
+        return $this->render('home/index.html.twig', compact('trainingAdulte', 'trainingEnfant', 'placeAdult', 'placeEnfant', 'nameList', 'userPlace'));
     }
 
     /**
@@ -53,15 +98,14 @@ class HomeController extends AbstractController
      */
     public function inscription(EntityManagerInterface $em, Training $training, User $user)
     {
-          $userTraining = new UserTraining;
-          $userTraining->setTraining($training);
-          $userTraining->setUser($user);
-          $userTraining->setDateRegistration(new DateTime('NOW'));
+        $userTraining = new UserTraining;
+        $userTraining->setTraining($training);
+        $userTraining->setUser($user);
+        $userTraining->setDateRegistration(new DateTime('NOW'));
 
-          $em->persist($userTraining);
-          $em->flush();
+        $em->persist($userTraining);
+        $em->flush();
 
-          return $this->redirectToRoute('home',['_fragment' => 'home-training']);         
-          
+        return $this->redirectToRoute('home', ['_fragment' => 'home-training']);
     }
 }
