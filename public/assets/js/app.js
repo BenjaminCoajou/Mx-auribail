95804354e21a4583f0acf084388643efdeb82df5
Benjamin Coajou <coajou.benjamin@gmail.com>
2021-06-12 15:21:41 +0200

    display dev page with konamcode

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -15,7 +15,29 @@ let app = {
            }
            lastPos = pos <= 0 ? 0 : pos;
        });
+
+       konamiCode();  
+       
     }
 };
+
+let secret = "1";
+function konamiCode() {
+ document.body.addEventListener("keyup", event => {
+   if (event.key) {
+         secret = secret + event.key;
+         console.log(secret);
+
+     let konamiCode = secret.indexOf("ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba");
+     if (konamiCode > 0) {
+         secret = "1";
+         document.location.href = "http://localhost/Mx-auribail/public/dev";
+     }
+   }
+ });
+}
+
+
+
 
 app.init();
