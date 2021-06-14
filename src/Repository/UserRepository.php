a544694891f1cb6021a0d32d3e034bafe1f7172b
Benjamin Coajou <coajou.benjamin@gmail.com>
2021-06-14 10:28:53 +0200

    display training, user and member with pills

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -36,22 +36,15 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->_em->flush();
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function findByRole($role)
     {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
+        return $this->createQueryBuilder('user')
+            ->andWhere('user.roles LIKE :role')
+            ->setParameter('role', '%'.$role.'%')
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?User
